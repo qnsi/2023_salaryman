31686707613908ba7268847e2673075c7ebcf007
Artur Kęsik <artur.kesik@tuatara.pl>
2023-10-05 15:54:31 +0200

    Very basic task adding and delete added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+type Task = {
+  name: string;
+};
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const addTask = (newTask: string) => {
+    setTasks((tasks: Task[]) => tasks.concat([{ name: newTask }]));
+  };
+
+  const deleteTask = (taskToDelete: Task) => {
+    setTasks((tasks) =>
+      tasks.filter((task) => task.name !== taskToDelete.name),
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddTask addTask={addTask} />
+      <TaskList tasks={tasks} deleteTask={deleteTask} />
     </div>
   );
 }
+
+const AddTask = ({ addTask }: { addTask: (newTask: string) => void }) => {
+  const [newTask, setNewTask] = useState("");
+
+  const handleAddTask = () => {
+    setNewTask("");
+    addTask(newTask);
+  };
+
+  return (
+    <div style={{ margin: "10px 10px" }}>
+      <input
+        value={newTask}
+        onChange={(event) => setNewTask(event.currentTarget.value)}
+      />
+      <button onClick={handleAddTask}>Add task</button>
+    </div>
+  );
+};
+
+const TaskList = ({
+  tasks,
+  deleteTask,
+}: {
+  tasks: Task[];
+  deleteTask: (task: Task) => void;
+}) => {
+  return (
+    <div>
+      {tasks.map((task) => {
+        return (
+          <div style={{ margin: "10px 10px" }}>
+            <span style={{ marginRight: 10 }}>{task.name}</span>
+            <button onClick={() => deleteTask(task)}>Delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
